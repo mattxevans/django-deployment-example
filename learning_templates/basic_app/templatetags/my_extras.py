d7722e91f734204fc101bd446d5505c228dc7719
Evans <mevans@silverstone.co.uk>
2020-10-19 16:47:36 +0100

    'second'

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -2,11 +2,11 @@ from django import template
 
 register = template.Library()
 
-@register.filer(name='cut')
-def cut(value,arg):
-    """
-    This cuts out all values of "arg" from string
-    """
-    return value.replace(arg,'')
+# @register.filer(name='cut')
+# def cut(value,arg):
+#     """
+#     This cuts out all values of "arg" from string
+#     """
+#     return value.replace(arg,'')
 
 # register.filter('cut',cut)
